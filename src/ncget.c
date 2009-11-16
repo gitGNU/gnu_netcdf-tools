c8ce81c31ec2e915e77e0d99e9a1c32fc636f944
Ivan Shmakov <ivan@main.uusia.org>
2009-11-16 23:57:18 +0600

    Added GNU GPL notices to the files, and the usual header, where missing.
    lib/numconv.c: Added a verbose GNU General Public License notice.
    lib/numconv.h: Likewise.
    lib/p_arg.c: Likewise.
    lib/p_arg.h: Likewise.
    lib/usemacro.h: Likewise.
    lib/useutil.c: Likewise.
    lib/useutil.h: Likewise.
    src/dump_array.c: Likewise.
    src/ncget.c: Likewise.
    src/ncget.h: Likewise.
    src/ncsel.c: Likewise.
    src/print_nc_info.c: Likewise.
    
    src/dump_array.c: Added usual header and footer, and the copyright
    holder info.
    src/ncget.c: Likewise.
    src/ncget.h: Likewise.
    src/ncsel.c: Likewise.
    src/print_nc_info.c: Likewise.

--- src/ncget.c
+++ src/ncget.c
@@ -1,6 +1,27 @@
+/*** ncget.c --- The ncget tool main ()  -*- C -*- */
 #define _GNU_SOURCE
 #define _ISOC99_SOURCE
 
+/*** Copyright (C) 2007, 2009 Ivan Shmakov */
+/*** Copyright (C) 2006 Anton Reznikov */
+
+/** This program is free software; you can redistribute it and/or modify
+ ** it under the terms of the GNU General Public License as published by
+ ** the Free Software Foundation; either version 2 of the License, or
+ ** (at your option) any later version.
+ **
+ ** This program is distributed in the hope that it will be useful, but
+ ** WITHOUT ANY WARRANTY; without even the implied warranty of
+ ** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ ** General Public License for more details.
+ **
+ ** You should have received a copy of the GNU General Public License
+ ** along with this program; if not, write to the Free Software
+ ** Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ ** 02110-1301 USA
+ */
+
+/*** Code: */
 #ifdef HAVE_CONFIG_H
 #include <config.h>
 #endif
@@ -380,3 +401,5 @@ int main (int argc, char *argv[])
 
   return 0;
 }
+
+/*** ncget.c ends here */
